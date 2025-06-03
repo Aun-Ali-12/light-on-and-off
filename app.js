ed2a33ce1eb8fce2e0a3e197edb4f8fdbf9298cb
Aun-Ali-12 <aunali29331@gmail.com>
2025-06-04 02:04:51 +0500

    light on and off

---
+++ app.js
@@ -0,0 +1,8 @@
+function bulb() {
+  let bulbOnn = document.getElementById("onn");
+  bulbOnn.classList.add("on");
+}
+function bulbOff(){
+let bulbOnn = document.getElementById("onn");
+  bulbOnn.classList.remove("on");
+}
