94693e19a1c1fa43b3719c06e5425b2a75fadbca
Aun-Ali-12 <aunali29331@gmail.com>
2025-06-09 15:23:50 +0500

    light on and off with button in dom js

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
+let circle = document.getElementById("circle");
+let body = document.getElementById("onn");
+let on = document.getElementById("onn-btn");
 function bulb() {
-  let bulbOnn = document.getElementById("onn");
-  bulbOnn.classList.add("on");
-}
-function bulbOff(){
-let bulbOnn = document.getElementById("onn");
-  bulbOnn.classList.remove("on");
+  circle.style.backgroundColor = "yellow";
+  let offBtn = document.createElement("button");
+  offBtn.innerText = "Off";
+  offBtn.style.padding = "0.5rem 0.6rem 0.5rem 0.6rem";
+  body.append(offBtn);
+  on.style.display = "none";
+  offBtn.onclick = function () {
+    circle.style.backgroundColor = "white";
+    offBtn.style.display = "none";
+    on.style.display = "block";
+  };
 }
